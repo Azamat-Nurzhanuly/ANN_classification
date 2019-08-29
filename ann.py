f6a5ca15babf9128719baa70cfe410093e924d47
Azamat Bekkhozha <azamatb@wimmersolutions.com>
2019-08-29 15:15:42 -0700

    Prediction and Evaluation

--- ann.py
+++ ann.py
@@ -54,3 +54,15 @@ classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
 # Compiling the  ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
+##### Prediction and Evaluation #####
+
+# Making prediction
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
