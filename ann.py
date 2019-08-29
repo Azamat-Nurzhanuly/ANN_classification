66da3eebc9b072451b9ed2d5750f6c8574235787
Azamat Bekkhozha <azamatb@wimmersolutions.com>
2019-08-29 14:01:47 -0700

    Encoding categorical data and resolving dummy variables trap

--- ann.py
+++ ann.py
@@ -5,5 +5,18 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13].values
+X = dataset.iloc[:, 3:13]
 y = dataset.iloc[:, 13].values
+
+# Encoding Categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+ct = ColumnTransformer(
+        [('oh_enc', OneHotEncoder(sparse=False), [1, 2]),],
+        remainder='passthrough'
+    )
+
+X = ct.fit_transform(X)
+
+# Avoid dummy variables trap
+X = np.delete(X, [0, 3], axis=1)
