5ec8e0b3b6353f03048831d744a7b320a1f9d159
Azamat Bekkhozha <azamatb@wimmersolutions.com>
2019-08-29 14:07:17 -0700

    Standard Scaling

--- ann.py
+++ ann.py
@@ -21,6 +21,12 @@ X = ct.fit_transform(X)
 # Avoid dummy variables trap
 X = np.delete(X, [0, 3], axis=1)
 
-#Splitting the dataset into the Training set and Test set
+# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+standardScaler = StandardScaler()
+X_train = standardScaler.fit_transform(X_train)
+X_test = standardScaler.transform(X_test)
