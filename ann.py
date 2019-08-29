536658291f5441650d0428661d60dc41bc154c38
Azamat Bekkhozha <azamatb@wimmersolutions.com>
2019-08-29 12:11:18 -0700

    importing the dataset

--- ann.py
+++ ann.py
@@ -1,3 +1,9 @@
+# Importing the libraries
 import numpy as np
 import matplotlib as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
